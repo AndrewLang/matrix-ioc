6f0698ed1e6d76640774070dacaf36e7d8594e23
Andy <nnlyx@hotmail.com>
2019-08-02 16:05:05 -0600

    add source dir to unit test project

--- tests/matrix-ioc-tests/main.cpp
+++ tests/matrix-ioc-tests/main.cpp
@@ -1,10 +1,23 @@
 #include "main.h"
+#include "Do.h"
 
 using namespace std;
+using namespace Matrix;
 
 int main()
 {
 	cout << "Unit tests for IOC." << endl;
+
+	int tryCount = 0;
+	auto lambda = [&](int x) {
+		tryCount = x;
+	};
+
+	Do<std::function<void(int)>> action(lambda);
+
+	action.attempt(4).sleep(100).start(5);
+
+	cout << "value " << tryCount << endl;
 
 	getchar();
 
