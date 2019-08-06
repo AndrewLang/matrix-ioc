bb7ae0ccfd8adcc48ffe551a3b178f72c2bf7612
Andy <nnlyx@hotmail.com>
2019-08-06 15:36:22 -0600

    test ioc method

--- tests/matrix-ioc-tests/test-demo.cpp
+++ tests/matrix-ioc-tests/test-demo.cpp
@@ -1,6 +1,30 @@
 #include "gtest/gtest.h"
+#include "Do.h"
+
+using namespace std;
+using namespace Matrix;
 
 TEST(TestSamples, VerifyGtest)
 {
 	EXPECT_EQ(1, 1);
+}
+
+
+TEST(TestSamples, VerifyGtestTrue)
+{
+	EXPECT_TRUE(true);
+}
+
+TEST(DoTests, TestDo)
+{
+	int tryCount = 0;
+	auto lambda = [&](int x) {
+		tryCount = x;
+	};
+
+	Do<std::function<void(int)>> action(lambda);
+
+	action.attempt(4).sleep(100).start(5);
+
+	cout << "value " << tryCount << endl;
 }
